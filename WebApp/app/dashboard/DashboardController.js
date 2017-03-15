a22d74d25bb6cc37b37d4414ad4b5a97bb493d92
Juliusb <Juliusb@ZYLUN-PC-0308>
2017-03-15 23:50:36 +0800

    angular 1.5 modal

---
+++ WebApp/app/dashboard/DashboardController.js
@@ -0,0 +1,27 @@
+﻿app.controller('DashboardController',
+    ['$scope', '$uibModal', function DashboardController($scope, $uibModal) {
+
+        $scope.modalClick = function () {
+           
+            var modalContent = $uibModal.open({
+                templateUrl: 'modal.html',
+                controller: 'ModalInstanceCtrl',
+                controllerAs: '$ctrl',
+                resolve: {
+                    items: function () {
+                      
+                    }
+                }
+            })
+
+
+        };
+
+    }]);
+
+app.controller('ModalInstanceCtrl', function ($uibModalInstance, $scope) {
+   
+    
+   
+
+});
